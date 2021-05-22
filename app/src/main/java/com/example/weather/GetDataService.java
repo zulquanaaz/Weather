ab953ee772dd1af549d7b31ea791e6e1420267de
harishmashaik <harishmashaikk@gmail.com>
2021-05-22 10:57:16 -0400

    Retrofit library and private Api key added.

---
+++ app/src/main/java/com/example/weather/GetDataService.java
@@ -0,0 +1,16 @@
+package com.example.weather;
+
+
+import com.example.weather.model.Example;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("forecast.json?key="+BuildConfig.API_KEY +"&q=Montreal&days=5&aqi=no&alerts=no/")
+    Call<Example> getMontrealWeather();
+
+
+}
---
+++ app/src/main/java/com/example/weather/RetrofitClientInstance.java
@@ -0,0 +1,24 @@
+package com.example.weather;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://api.weatherapi.com/v1/";
+
+
+    public  static Retrofit getRetrofitInstance()
+    {
+        if (retrofit == null)
+        {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
